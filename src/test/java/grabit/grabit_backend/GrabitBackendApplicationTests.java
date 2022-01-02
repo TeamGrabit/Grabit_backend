61199bcac96a4b821ad0140b55d7f18a49a80663
qf9ar8nv <ckguscjf0@gmail.com>
2022-01-02 15:26:36 +0900

    spring boot project

---
+++ src/test/java/grabit/grabit_backend/GrabitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package grabit.grabit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
