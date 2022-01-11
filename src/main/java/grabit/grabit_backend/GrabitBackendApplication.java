f46c00cc961c529823edeb441da8e3f0d8385406
qf9ar8nv <ckguscjf0@gmail.com>
2022-01-11 14:12:22 +0900

    connect remote MYSQL service

--- src/main/java/grabit/grabit_backend/GrabitBackendApplication.java
+++ src/main/java/grabit/grabit_backend/GrabitBackendApplication.java
@@ -1,13 +1,21 @@
 package grabit.grabit_backend;
 
+import grabit.grabit_backend.Domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
 
 @SpringBootApplication
 public class GrabitBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrabitBackendApplication.class, args);
+
+
 	}
 
 }
