e059096a19c6f34fce3a9e39c9ac2bd6f5f769ee
sally0226 <sally734595@gmail.com>
2022-02-12 15:56:27 +0900

    !FIX: corsFilter 추가
    - 프론트에서 요청시 발생하는 cors 에러를 해결하기 위해 corsFilter추가

--- src/main/java/grabit/grabit_backend/GrabitBackendApplication.java
+++ src/main/java/grabit/grabit_backend/GrabitBackendApplication.java
@@ -21,8 +21,8 @@ public class GrabitBackendApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**")
 						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowCredentials(false);
+						.allowedMethods("*");
+//						.allowCredentials(false);
 
 			}
 		};
