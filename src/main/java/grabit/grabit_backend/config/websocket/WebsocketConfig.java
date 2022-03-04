81326fbab7e8a5b3f370ba88c8e25070c1096cec
qf9ar8nv <ckguscjf0@gmail.com>
2022-03-04 15:49:41 +0900

    독립적인 chatting Room 설정

--- src/main/java/grabit/grabit_backend/config/websocket/WebsocketConfig.java
+++ src/main/java/grabit/grabit_backend/config/websocket/WebsocketConfig.java
@@ -1,9 +1,13 @@
 package grabit.grabit_backend.config.websocket;
 
 import grabit.grabit_backend.handler.ChattingHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Configuration
+@EnableWebSocket
 public class WebsocketConfig implements WebSocketConfigurer {
 	private final ChattingHandler chattingHandler;
 
--- src/main/java/grabit/grabit_backend/handler/ChattingHandler.java
+++ src/main/java/grabit/grabit_backend/handler/ChattingHandler.java
@@ -1,7 +1,7 @@
 package grabit.grabit_backend.handler;
 
-import com.mysql.cj.Session;
-import grabit.grabit_backend.AOP.APILoggingAspect;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import grabit.grabit_backend.Domain.Chatting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,6 +11,7 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,30 +19,41 @@ import java.util.Map;
 public class ChattingHandler extends TextWebSocketHandler {
 	private static final Logger logger = LoggerFactory.getLogger(TextWebSocketHandler.class);
 
-	private static List<Map<String, Session>> sessionList = new ArrayList<>();
+	private static Map<Long, List<WebSocketSession>> sessionList = new HashMap<>();
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		// 메세지 처리.
 		String payload = message.getPayload();
 
+		// Json to Chatting Object
+		ObjectMapper objectMapper = new ObjectMapper();
+		Chatting chatting = objectMapper.readValue(payload, Chatting.class);
 
-	}
-
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// 클라이언트 접속 후 처리.
-		super.afterConnectionEstablished(session);
-
-		// list.add(session);
-
+		if(chatting.getChattingType().equals("ENTER")){
+			if(!sessionList.containsKey(chatting.getChallengeId())){
+				List<WebSocketSession> sessList = new ArrayList<>();
+				sessList.add(session);
+				sessionList.put(chatting.getChallengeId(), sessList);
+			} else{
+				List<WebSocketSession> sessList = sessionList.get(chatting.getChallengeId());
+				sessList.add(session);
+			}
+		}else if(chatting.getChattingType().equals("MESSAGE")){
+			List<WebSocketSession> sessList = sessionList.get(chatting.getChallengeId());
+			for(WebSocketSession sess : sessList){
+				sess.sendMessage(message);
+			}
+		}
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		// 클라이언트 접속 해제 후 처리
-		super.afterConnectionClosed(session, status);
-
-		// list.remove(session);
+		for(Long chattingId : sessionList.keySet()){
+			List<WebSocketSession> sessList = sessionList.get(chattingId);
+			if(sessList.contains(session)){
+				sessList.remove(session);
+			}
+		}
 	}
 }
