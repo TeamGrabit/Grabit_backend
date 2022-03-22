5471e4501ec94a88ebbf2161d8daaf1935254bef
qf9ar8nv <ckguscjf0@gmail.com>
2022-03-22 20:30:04 +0900

    [추가] N+1 문제 해결
    findALL 사용시 불필요하게 많이 나가던 쿼리 제거

--- src/main/java/grabit/grabit_backend/repository/ChallengeRepository.java
+++ src/main/java/grabit/grabit_backend/repository/ChallengeRepository.java
@@ -2,6 +2,7 @@ package grabit.grabit_backend.repository;
 
 import grabit.grabit_backend.domain.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,10 @@ import java.util.Optional;
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 	Challenge save(Challenge challenge);
-	Optional<Challenge> findById(Long id);
 	List<Challenge> findByName(String name);
-	List<Challenge> findAll();
 	void deleteById(Long id);
+	Optional<Challenge> findById(Long id);
+
+	@Query("select distinct c from challenge c join fetch c.userChallengeList")
+	List<Challenge> findAll();
 }
