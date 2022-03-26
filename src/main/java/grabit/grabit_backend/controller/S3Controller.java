b1b7149b4c96887d9b47a57233232c84bcc9bd38
qf9ar8nv <ckguscjf0@gmail.com>
2022-03-26 16:27:52 +0900

    [수정] base_url 수정
    prefix_url 설정을 변경해서 base_url을 수정함

--- src/main/java/grabit/grabit_backend/controller/S3Controller.java
+++ src/main/java/grabit/grabit_backend/controller/S3Controller.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("api/s3")
+@RequestMapping("image")
 public class S3Controller {
 
 	private S3Service s3Service;
