a5ee899bf7c0174deb9585ee3b1b3e073e1d2d7d
sally0226 <sally734595@gmail.com>
2022-03-30 23:40:36 +0900

    chore: dto에서 setter 제거

--- src/main/java/grabit/grabit_backend/dto/UpdateUserDTO.java
+++ src/main/java/grabit/grabit_backend/dto/UpdateUserDTO.java
@@ -1,15 +1,9 @@
 package grabit.grabit_backend.dto;
 
-import grabit.grabit_backend.domain.User;
 import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Getter
-@Setter
 public class UpdateUserDTO {
     @Min(value = 1)
     private int id;
