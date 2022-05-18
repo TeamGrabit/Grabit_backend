90db4fc835fd0644e6f95a5be77574f57a0e9361
qf9ar8nv <ckguscjf0@gmail.com>
2022-05-18 19:24:29 +0900

    set Local, Dev Setting, S3mock Configuration
    local에서 사용할 S3mock 객체의 configuration을 따로 정의함

--- src/main/java/grabit/grabit_backend/config/aws/S3Config.java
+++ src/main/java/grabit/grabit_backend/config/aws/S3Config.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 
-@Profile("DATABASE")
+@Profile("STORAGE")
 @Configuration
 public class S3Config {
 
--- src/main/java/grabit/grabit_backend/config/aws/S3MockService.java
+++ src/main/java/grabit/grabit_backend/config/aws/S3MockService.java
@@ -17,8 +17,6 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class S3MockService {
 
-	private S3Mock s3Mock;
-
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
@@ -28,27 +26,13 @@ public class S3MockService {
 	@Value("${cloud.aws.s3.mock.port}")
 	private int port;
 
-	public S3MockService(S3Mock s3Mock) {
-		this.s3Mock = s3Mock;
-		s3Mock.start();
-	}
-
-	@Bean
-	public S3Mock s3Mock() {
-		return new S3Mock.Builder()
-				.withPort(port)
-				.withInMemoryBackend()
-				.build();
-	}
-
-	@Bean
+	@PostConstruct
 	public AmazonS3 amazonS3() {
 		AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(getUri(), region);
 		AmazonS3 amazonS3 = AmazonS3ClientBuilder
 				.standard()
 				.withPathStyleAccessEnabled(true)
 				.withEndpointConfiguration(endpoint)
-				.withRegion(region)
 				.withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
 				.build();
 		amazonS3.createBucket(bucket);
