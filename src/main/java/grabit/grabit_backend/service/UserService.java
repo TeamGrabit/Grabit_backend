f6cddd9f9850162d5a1fd62f72bbae427dccacba
daisy-kim <sally0226@naver.com>
2022-07-10 17:02:14 +0900

    fix: update user api
    - request body에서 id 제거하고, 본인의 유저 정보를 바꾸는 방향으로 수정

--- src/main/java/grabit/grabit_backend/service/UserService.java
+++ src/main/java/grabit/grabit_backend/service/UserService.java
@@ -25,16 +25,10 @@ public class UserService {
      */
     @Transactional
     public User updateUser(UpdateUserDTO updateUserDTO, User user) {
-        // 권한 확인 (본인인지)
-        if (updateUserDTO.getId() != user.getId()) {
-            throw new ForbiddenException();
-        }
-        // update 진행
         if (updateUserDTO.getUsername() != null) user.setUsername(updateUserDTO.getUsername());
         if (updateUserDTO.getBio() != null) user.setBio(updateUserDTO.getBio());
         if (updateUserDTO.getProfileImage() != null) user.setProfileImg(updateUserDTO.getProfileImage());
 
-        // return
         return userRepository.save(user);
     }
 }
